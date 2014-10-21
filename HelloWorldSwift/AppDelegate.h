e96970de9d931a95a9dd045153ead5c5f36b1565
water <water@waterdeMac-mini.local>
2014-10-21 16:42:47 +0800

    Initial Commit

---
+++ HelloWorldSwift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorldSwift
+//
+//  Created by water on 14-10-21.
+//  Copyright (c) 2014年 water. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorldSwift/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorldSwift
+//
+//  Created by water on 14-10-21.
+//  Copyright (c) 2014年 water. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
